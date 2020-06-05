0a809d8f049f6b358087030edc0c6a0940433e9c
Korti <lucas.korthauer@gmail.com>
2020-06-05 17:31:54 +0200

    Added missing endpoints to the recipe service.

--- apps/recipe-service/src/app/recipe/recipe.controller.ts
+++ apps/recipe-service/src/app/recipe/recipe.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern } from "@nestjs/microservices";
 import { RecipeService } from './recipe.service';
-import { Recipe, RecipeInput } from "@recipe-book/entities";
+import { Recipe, RecipeInput, RecipeUpdate, IngredientAdd, IngredientUpdate, IngredientRemove } from "@recipe-book/entities";
 
 @Controller()
 export class RecipeController {
@@ -25,9 +25,33 @@ export class RecipeController {
         return this.recipeService.addRecipe(input);
     }
 
-    @MessagePattern({ cmd: "remove"})
+    @MessagePattern({ cmd: "remove" })
     async removeRecipe(id: number): Promise<boolean> {
         Logger.log(`Request to remove recipe with ID: ${id}`);
         return this.recipeService.removeRecipe(id);
+    }
+
+    @MessagePattern({ cmd: "update" })
+    async updateRecipe(input: RecipeUpdate): Promise<Recipe> {
+        Logger.log(`Request to update recipe with ID: ${input.id}`);
+        return this.recipeService.updateRecipe(input);
+    }
+
+    @MessagePattern({ cmd: "addIngredient" })
+    async addIngredient(input: IngredientAdd): Promise<Recipe> {
+        Logger.log(`Request to add ingredient to recipe with ID: ${input.recipeId}`);
+        return this.recipeService.addIngredient(input);
+    }
+
+    @MessagePattern({ cmd: "updateIngredient" })
+    async updateIngredient(input: IngredientUpdate): Promise<Recipe> {
+        Logger.log(`Request to update ingredient with ID: ${input.ingredientId} on recipe with ID: ${input.recipeId}`);
+        return this.recipeService.updateIngredient(input);
+    }
+
+    @MessagePattern({ cmd: "removeIngredient" })
+    async removeIngredient(input: IngredientRemove): Promise<boolean> {
+        Logger.log(`Request to remove ingredient with ID: ${input.ingredientId} on recipe with ID: ${input.recipeId}`);
+        return this.recipeService.removeIngredient(input);
     }
 }
