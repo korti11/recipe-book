b197e681c34bcfdc898388e0f51b411f7fd96c8c
Korti <lucas.korthauer@gmail.com>
2020-06-05 17:08:06 +0200

    Added endpoints to the recipe service. Endpoints: add new ingredient, update recipe/ingredient, remove ingredient.

--- apps/recipe-service/src/app/recipe/recipe.service.ts
+++ apps/recipe-service/src/app/recipe/recipe.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Recipe, RecipeInput } from "@recipe-book/entities";
+import { Recipe, RecipeInput, RecipeUpdate, Ingredient, IngredientAdd, IngredientUpdate, IngredientRemove } from "@recipe-book/entities";
 
 @Injectable()
 export class RecipeService {
 
     private recipeID = 0;
+    private ingredientID = 0;
     private testData: Recipe[] = [
         {
             id: this.recipeID++,
@@ -12,6 +13,7 @@ export class RecipeService {
             description: "",
             ingredients: [
                 {
+                    id: this.ingredientID++,
                     name: "Toast",
                     amount: 1,
                     unit: "Stk."
@@ -36,6 +38,7 @@ export class RecipeService {
         return new Promise<Recipe>((res, rej) => {
             let recipe: Recipe = input as Recipe;
             recipe.id = this.recipeID++;
+            recipe.ingredients.forEach((i) => i.id = this.ingredientID++);
             this.testData.push(recipe);
             res(recipe);
         });
@@ -46,6 +49,46 @@ export class RecipeService {
             let newData: Recipe[] = this.testData.filter((r) => r.id !== id);
             let removed: boolean = newData.length < this.testData.length;
             this.testData = newData;
+            res(removed);
+        });
+    }
+
+    async updateRecipe(data: RecipeUpdate): Promise<Recipe> {
+        return new Promise<Recipe>((res, rej) => {
+            let recipe: Recipe = this.testData.filter((r) => r.id === data.id)[0];
+            recipe.title = data.title ?? recipe.title;
+            recipe.description = data.description ?? recipe.description;
+            res(recipe);
+        });
+    }
+
+    async addIngredient(data: IngredientAdd): Promise<Recipe> {
+        return new Promise<Recipe>((res, rej) => {
+            let recipe: Recipe = this.testData.filter((r) => r.id === data.recipeId)[0];
+            let ingredient: Ingredient = data.ingredient as Ingredient;
+            ingredient.id = this.ingredientID++;
+            recipe.ingredients.push(ingredient);
+            res(recipe);
+        });
+    }
+
+    async updateIngredient(data: IngredientUpdate): Promise<Recipe> {
+        return new Promise<Recipe>((res, rej) => {
+            let recipe: Recipe = this.testData.filter((r) => r.id === data.recipeId)[0];
+            let ingredient: Ingredient = recipe.ingredients.filter((i) => i.id === data.ingredientId)[0];
+            ingredient.name = data.name ?? ingredient.name;
+            ingredient.amount = data.amount ?? ingredient.amount;
+            ingredient.unit = data.unit ?? ingredient.unit;
+            res(recipe);
+        });
+    }
+
+    async removeIngredient(data: IngredientRemove): Promise<boolean> {
+        return new Promise<boolean>((res, rej) => {
+            let recipe: Recipe = this.testData.filter((r) => r.id === data.recipeId)[0];
+            let newIngredients: Ingredient[] = recipe.ingredients.filter((i) => i.id !== data.recipeId);
+            let removed: boolean = newIngredients.length < recipe.ingredients.length;
+            recipe.ingredients = newIngredients;
             res(removed);
         });
     }
--- libs/entities/src/lib/entities.ts
+++ libs/entities/src/lib/entities.ts
@@ -2,6 +2,9 @@ import { ObjectType, Field, Int, Float, InputType } from "@nestjs/graphql";
 
 @ObjectType()
 export class Ingredient {
+  @Field(type => Int)
+  id: number;
+
   @Field({ nullable: false })
   name: string;
 
@@ -51,3 +54,50 @@ export class RecipeInput {
   ingredients: IngredientInput[];
 }
 
+@InputType()
+export class RecipeUpdate {
+  @Field(type => Int)
+  id: number;
+
+  @Field({ nullable: true })
+  title?: string;
+
+  @Field({ nullable: true})
+  description?: string;
+}
+
+@InputType()
+export class IngredientAdd {
+  @Field(type => Int)
+  recipeId: number;
+
+  @Field(type => IngredientInput)
+  ingredient: IngredientInput;
+}
+
+@InputType()
+export class IngredientUpdate {
+  @Field(type => Int)
+  recipeId: number;
+
+  @Field(type => Int)
+  ingredientId: number;
+
+  @Field({ nullable: true })
+  name?: string;
+
+  @Field(type => Float, { nullable: true })
+  amount?: number;
+
+  @Field({ nullable: true })
+  unit?: string;
+}
+
+@InputType()
+export class IngredientRemove {
+  @Field(type => Int)
+  recipeId: number;
+
+  @Field(type => Int)
+  ingredientId: number;
+}
